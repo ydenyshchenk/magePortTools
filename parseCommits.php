c9c2cb71270f5774bb57e2377e98de06fbcd37e2
Yuriy Denyshchenko <ydenyshchenk@magento.com>
2018-01-23 13:28:56 +0200

    Added batch functionality

--- parseCommits.php
+++ parseCommits.php
@@ -1,39 +1,63 @@
 #!/usr/bin/env php
 <?php
 
+define("DS", DIRECTORY_SEPARATOR);
+
 if (empty($argv[1])) {
     exit("Please clarify branch name for search\n");
 }
 
-$branch = $argv[1];
-
 $mode = (!empty($argv[2]) && preg_match('/commit/', $argv[2])) ? 'commits' : 'patch';
+$options = '';
 
-$gitLog = shell_exec("git log --all --grep='$branch'");
-
-preg_match_all('/commit\s([^\s\n]*)\n([^\n]*\n){1}Date\:\s+([^\n]*)/', $gitLog, $matches);
-
-$commits = [];
-
-foreach ($matches[1] as $i => $commit) {
-    $unixTime = strtotime($matches[3][$i]);
-    $commits[$unixTime] = $commit;
+if (!in_array('not-all', $argv)) {
+    $options .= ' --all ';
 }
 
-ksort($commits);
-
-$patch = '';
-$patchArr = [];
-
-foreach ($commits as $c) {
-    $patchArr[] = shell_exec("git diff $c~1 $c");
-
-//    $b = 1;
-//    echo "$c\n";
+$diffFileDir = '_DIFFS' . DS . time();
+if (!file_exists($diffFileDir)) {
+    mkdir($diffFileDir, 0777, true);
 }
 
-if ($mode == 'commits') {
-    echo implode("\n", $commits) . "\n";
-} else {
-    echo implode('', $patchArr);
+$branchesRaw = $argv[1];
+$branches = explode(',', $branchesRaw);
+sort($branches);
+
+foreach ($branches as $branch) {
+    $gitLog = shell_exec("git log $options --grep='$branch'");
+    preg_match_all('/commit\s([^\s\n]*)\n([^\n]*\n){1}Date\:\s+([^\n]*)/', $gitLog, $matches);
+
+    $commits = [];
+    foreach ($matches[1] as $i => $commit) {
+        $unixTime = strtotime($matches[3][$i]);
+        $commits[$unixTime] = $commit;
+    }
+
+    ksort($commits);
+    $patch = '';
+    $patchArr = [];
+
+    foreach ($commits as $c) {
+        $patchArr[] = shell_exec("git diff $c~1 $c");
+    }
+
+    $diffContent = implode('', $patchArr);
+    $commitsContent = implode("\n", $commits) . "\n";
+
+    $branchFile = $diffFileDir . DS . trim($branch, " \t\n\r\0\x0B:");
+    $branchFileDiff = $branchFile . '.diff';
+    $branchFileCommits = $branchFile . '.commits.log';
+    if (is_writeable($diffFileDir)) {
+        file_put_contents($branchFileDiff, $diffContent);
+        file_put_contents($branchFileCommits, $commitsContent);
+    } else {
+        error_log("=== $branch: diff ===", 0);
+        error_log($diffContent, 0);
+        error_log("=== $branch: commits ===", 0);
+        error_log($commitsContent, 0);
+    }
+
+    error_log($branch . ' OK', 0);
 }
+
+error_log("BRANCHES:\n" . implode("\n", $branches), $diffFileDir . DS . 'branches.log');
